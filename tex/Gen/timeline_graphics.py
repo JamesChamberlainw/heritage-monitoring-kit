961f1198574fd8607e2adb10cfd8e02959b4c871
James William Chamberlain <jw.chamberlain@outlook.com>
2025-03-31 21:05:34 +0100

    Delete timeline_graphics.py

--- tex/Gen/timeline_graphics.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-start_date = datetime(2025, 3, 3) # week of first meeting 
-num_weeks = 25  # 23 weeks 4 days (deadline 15th of August 2025)
-
-for i in range(num_weeks):
-    date = start_date + timedelta(weeks=i)
-    formatted = date.strftime("%B %#d") 
-    print(f"\\gantttitle{{{formatted} (W{i+1})}}{{1}}")
